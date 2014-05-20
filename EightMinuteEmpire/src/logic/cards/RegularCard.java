cb6bad48fd957a450de293a6114fe6f8b448ec29
nunoaguiarng@gmail.com <nunoaguiarng@gmail.com@b732fad8-a4ff-ab3c-e606-e639d78c5a28>
2014-05-20 23:14:50 +0000

    Já grava e carrega o jogo... teoricamente. implements Serializable! implements Serializable! Até os estados têm de estar implements Serializable!

--- EightMinuteEmpire/src/logic/cards/RegularCard.java
+++ EightMinuteEmpire/src/logic/cards/RegularCard.java
@@ -3,15 +3,19 @@ package logic.cards;
 import java.io.Serializable;
 import logic.actions.BaseAction;
 
-public class RegularCard implements Serializable{
+public class RegularCard implements Serializable {
+
     String resource;
     int resourceNumber;
     BaseAction action;
+    static int idc = 0;
+    int id;
 
     public RegularCard(String resource, int resourceNumber, BaseAction action) {
         this.resource = resource;
         this.resourceNumber = resourceNumber;
         this.action = action;
+        id = idc++;
     }
 
     public String getResource() {
@@ -25,9 +29,12 @@ public class RegularCard implements Serializable{
     public BaseAction getAction() {
         return action;
     }
-    
-    public String toString()
-    {
+
+    public String toString() {
         return resource + " card with " + action.toString();
+    }
+
+    public int getID() {
+        return id;
     }
 }
