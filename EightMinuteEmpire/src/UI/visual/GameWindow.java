da41b227ef2c96b139fa2ae58cce49b25b5c3ba3
a21210392@gmail.com <a21210392@gmail.com@b732fad8-a4ff-ab3c-e606-e639d78c5a28>
2014-05-27 13:50:36 +0000

    Cenas

--- EightMinuteEmpire/src/UI/visual/GameWindow.java
+++ EightMinuteEmpire/src/UI/visual/GameWindow.java
@@ -2,9 +2,22 @@ package UI.visual;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.ComponentOrientation;
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -18,24 +31,28 @@ public final class GameWindow extends JFrame {
     JButton useCard;
     JButton endTurn;
     List<JLabel> players;
+    List<JPanel> cards;
+    BufferedImage image;
+    boolean hasColor;
 
     public GameWindow(int x, int y, String title) {     // RECEBE Game
         super(title);
         setLocation(x, y);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(false);
-        setSize(656, 450);
+        setSize(1024, 600);
         buildComponents();                              // RECEBE Game
         setComponents();
         addListeners();
+        hasColor = false;
     }
 
     public void buildComponents() {
         useCard = new JButton("Use Card");
-        useCard.setSize(80, 27);
+        useCard.setPreferredSize(new Dimension(80, 27));
 
         endTurn = new JButton("End Turn");
-        endTurn.setSize(80, 27);
+        endTurn.setPreferredSize(new Dimension(80, 27));
 
         // INICIALIZA Game.getPlayers().size() LABELS
 
@@ -53,8 +70,8 @@ public final class GameWindow extends JFrame {
                 super.paint(g);
             }
         };
-        mainBottom.setBackground(Color.blue);
-        mainBottom.setSize(526, 107);
+        mainBottom.setBackground(Color.black);
+        mainBottom.setPreferredSize(new Dimension(526, 177));
 
         mainLeft = new JPanel() {
             @Override
@@ -63,19 +80,86 @@ public final class GameWindow extends JFrame {
             }
         };
         mainLeft.setBackground(Color.green);
-        mainLeft.setSize(120, 427);
+        mainLeft.setPreferredSize(new Dimension(120, 427));
+
+        cards = new ArrayList<>();
+
+        try {
+            image = ImageIO.read(new File("C:\\Users\\L\\Desktop\\ISEC\\2º Semestre\\Programação Avançada\\Trabalho Prático\\Projecto\\EightMinuteEmpire\\00_place_army_3.png"));
+        } catch (IOException ex) {
+            Logger.getLogger(GameWindow.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        for (int i = 0; i < 6; i++) {
+            JPanel jb = new JPanel() {
+                @Override
+                public void paint(Graphics g) {
+                    super.paint(g);
+                    if (image != null) {
+                        g.drawImage(image, 6, 0, 129, 178, null);
+                    } else {
+                        g.drawString("Image failed to load", 30, 30);
+                    }
+                }
+            };
+            jb.setBackground(Color.black);
+            jb.setMinimumSize(new Dimension(128, 177));
+            jb.setPreferredSize(new Dimension(128, 177));
+            jb.setMaximumSize(new Dimension(128, 177));
+
+            cards.add(jb);
+        }
     }
 
     public void setComponents() {
         Container cp = getContentPane();
 
         cp.setLayout(new BorderLayout());
+        cp.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
         cp.add(mainPanel, BorderLayout.CENTER);
-        cp.add(mainBottom, BorderLayout.PAGE_END);
-        cp.add(mainLeft, BorderLayout.LINE_START);
+        cp.add(mainLeft, BorderLayout.WEST);
+
+        mainPanel.setLayout(new BorderLayout());
+        mainPanel.add(mainBottom, BorderLayout.SOUTH);
+
+        mainBottom.setLayout(new GridLayout(1, 6, 10, 10));
+        mainBottom.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+
+        for (JPanel jb : cards) {
+            mainBottom.add(jb);
+        }
+
         // DISPOR LABELS PROGRESSIVAMENTE, DEPENDE DO NUMPLAYERS
     }
 
     public void addListeners() {
+        for (final JPanel jb : cards) {
+            jb.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseEntered(MouseEvent e) {
+                    if (!hasColor) {
+                        jb.setBackground(Color.red);
+                    }
+                }
+
+                @Override
+                public void mouseExited(MouseEvent e) {
+                    if (!hasColor) {
+                        jb.setBackground(Color.black);
+                    }
+                }
+
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    if (hasColor) {
+                        jb.setBackground(Color.black);
+                        hasColor = false;
+                    } else {
+                        jb.setBackground(Color.green);
+                        hasColor = true;
+                    }
+                }
+            });
+        }
     }
 }
--- EightMinuteEmpire/src/UI/visual/mainVisual.java
+++ EightMinuteEmpire/src/UI/visual/mainVisual.java
@@ -4,6 +4,6 @@ public class mainVisual {
     public static void main(String[] args) {
         GameWindow gw = new GameWindow(50, 50, "Eight Minute Empire");
         
-        gw.show();
+        gw.setVisible(true);
     }
 }
