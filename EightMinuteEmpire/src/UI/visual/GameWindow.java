11f6df2ac858b22ba7bd69ac4916572d4e9dfec6
a21210392@gmail.com <a21210392@gmail.com@b732fad8-a4ff-ab3c-e606-e639d78c5a28>
2014-06-12 18:03:55 +0000

    Senão queixa-se

--- EightMinuteEmpire/src/UI/visual/GameWindow.java
+++ EightMinuteEmpire/src/UI/visual/GameWindow.java
@@ -9,7 +9,6 @@ import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
